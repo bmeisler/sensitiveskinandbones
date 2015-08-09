ca845785b74cb99b375713a82e22d0a204b8025a
Bernard Meisler <bmeisler@gmail.com>
2015-08-08 23:42:25 -0700

    added old header files to deprecated folder

--- header/categoriesMenu.php
+++
@@ -1,6 +0,0 @@
-
-<div id="menuWrapper">
-<div id="access" class="sticky" role="navigation">
-	<?php wp_nav_menu( array( 'container_class' => 'categories', 'theme_location' => 'secondary' ) ); ?>
-</div>
-</div><!--#access-->
--- header/pagesMenu.php
+++
@@ -1,5 +0,0 @@
-<!--Pages Menu - About, Store, Books Back Issues-->
-
-<div id="access"  role="navigation">
-   <?php wp_nav_menu( array( 'container_class' => 'pages', 'theme_location' => 'primary' ) ); ?>
-</div><!-- #access -->
